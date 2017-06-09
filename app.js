9d037da75e3558a33071a0fb1d4a11b1429dab3b
Ridge <rto@MBAir.local>
2017-06-08 17:50:00 -0700

    clean up and add text instructionsto app.js

--- app.js
+++ app.js
@@ -13,14 +13,12 @@ export default class ReactNativeFirebase extends Component {
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>
-          Welcome to React Native!
+          Welcome to Firebase Quickstart!
         </Text>
         <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
+          To get started, edit ./firebaseConfig.js
+          by adding in your firebase app credentials from
+          console.firebase.google.com > Your App > Add Firebase to your web app
         </Text>
       </View>
     );
@@ -42,7 +40,8 @@ const styles = StyleSheet.create({
   instructions: {
     textAlign: 'center',
     color: '#333333',
-    marginBottom: 5,
+    marginRight: 10,
+    marginLeft: 10
   },
 });
 
